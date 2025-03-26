647b9ac1960dd51fc86b885c092af99416f80b4b
ahmedsami1 <asamiix1@gmail.com>
2025-03-26 04:37:44 +0200

    Initial commit

---
+++ lib/Features/splash/presentaion/views/widgets/splash_view_body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      decoration: const BoxDecoration(
+        color: Color( 0xFF100B20)
+      ),
+      child: const Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}

a146692b9aaf75359c0abe188f5a4d307121f46b
ahmedsami1 <asamiix1@gmail.com>
2025-03-26 06:24:05 +0200

    change theme & add text to splash view

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ class Bookly extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  GetMaterialApp(
-      theme: ThemeData().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
       ),
       debugShowCheckedModeBanner: false,
       home: const SplashView(),

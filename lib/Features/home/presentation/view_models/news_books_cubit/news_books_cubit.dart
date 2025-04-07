f388fe3f861fa51af49f8cb217745876dd61b711
ahmedsami1 <asamiix1@gmail.com>
2025-04-07 10:18:44 +0200

    setup service locator

--- lib/Features/home/presentation/view_models/news_books_cubit/news_books_cubit.dart
+++ lib/Features/home/presentation/view_models/news_books_cubit/news_books_cubit.dart
@@ -1,8 +1,8 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
 
 part 'news_books_state.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/Features/home/presentation/view_models/feature_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/Features/home/presentation/view_models/news_books_cubit/news_books_cubit.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/constants.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -12,13 +17,19 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>()),),
+        BlocProvider(create: (context)=> NewsBooksCubit(getIt.get<HomeRepoImpl>()),),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
